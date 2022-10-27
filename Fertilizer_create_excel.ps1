a3472086cc9d180b40ac0926a875c465c52cf635
OBULASETTI VENKANNA BABU <38856533+venkanna831@users.noreply.github.com>
2022-10-27 10:04:13 +0530

    Add files via upload

---
+++ Fertilizer_create_excel.ps1
@@ -0,0 +1,52 @@
+﻿$month=$args[0]
+$year=$args[1]
+[int]$from=$args[2]
+[int]$to=$args[3]
+#Write-Host $from.GetType()
+$name="Dhanalkshmi_F&P_Fertilizers_Sale_Report_"+$month+"_"+$year
+$excel = New-Object -ComObject excel.application 
+$excel.visible = $True
+$workbook = $excel.Workbooks.Add()
+$uregwksht= $workbook.Worksheets.Item(1) 
+$outputpath=$PSScriptRoot+"\Fertilizers\$name"
+$uregwksht.Name = 'Form'
+$row = 1 
+$Column = 1
+$uregwksht.Cells.Item($row,$column)= $name
+$MergeCells = $uregwksht.Range('A1:F2')
+$MergeCells.Select() 
+$MergeCells.MergeCells = $true 
+$uregwksht.Cells(1, 1).HorizontalAlignment = -4108
+$uregwksht.Cells.Item(1,1).Font.Size = 16 
+$uregwksht.Cells.Item(1,1).Font.Bold=$True 
+#$uregwksht.Cells.Item(1,1).Font.Name = 'Times New Roman' 
+#$uregwksht.Cells.Item(1,1).Font.ThemeFont = 1 
+#$uregwksht.Cells.Item(1,1).Font.ThemeColor = 4 
+#$uregwksht.Cells.Item(1,1).Font.ColorIndex = 55 
+#$uregwksht.Cells.Item(1,1).Font.Color = 8210719
+$uregwksht.Cells.Item(3,1)  = 'BILL NUMBER'
+$uregwksht.Cells.Item(3,1).Font.Bold=$True 
+$uregwksht.Cells.Item(3,2) = 'CREDIT' 
+$uregwksht.Cells.Item(3,2).Font.Bold=$True 
+$uregwksht.Cells.Item(3,3)  = 'CASH' 
+$uregwksht.Cells.Item(3,3).Font.Bold=$True 
+$uregwksht.Cells.Item(3,4) = '12%'
+$uregwksht.Cells.Item(3,4).Font.Bold=$True 
+$uregwksht.Cells.Item(3,5) = '18%'
+$uregwksht.Cells.Item(3,5).Font.Bold=$True 
+$uregwksht.Cells.Item(3,6)= 'RETURN BILLS'
+$uregwksht.Cells.Item(3,6).Font.Bold=$True 
+$usedRange = $uregwksht.UsedRange
+$uregwksht.Cells.Item(1,1).Font.Size = 12
+$usedRange.EntireColumn.ColumnWidth=12
+$i=4
+
+$j=$to - $from
+$j+=$i
+for($i=4;$i -le $j;$i++){
+
+$uregwksht.Cells.Item($i,1)= $from
+$from+=1
+}
+$workbook.SaveAs($outputpath) 
+$excel.Quit()
---
+++ Pesticides_create_report.ps1
@@ -0,0 +1,52 @@
+﻿$month=$args[0]
+$year=$args[1]
+[int]$from=$args[2]
+[int]$to=$args[3]
+#Write-Host $from.GetType()
+$name="Dhanalkshmi_F&P_Pesticides_Sale_Report_"+$month+"_"+$year
+$fname=$name+".xlsx"
+$excel = New-Object -ComObject excel.application 
+$path=$PSScriptRoot+"\Pesticides\"+$fname
+$wb = $excel.Workbooks.Open($path)
+$ExcelWorkSheet = $wb.Sheets.Item("Form")
+$i=4
+$j=$to - $from
+$j+=$i
+[int]$credit_total=0
+[int]$cash_total=0
+[int]$return_total=0
+for($i=4;$i -le $j;$i++){
+$credit_total+=$ExcelWorkSheet.cells.Item($i, 2).value2
+$cash_total+=$ExcelWorkSheet.cells.Item($i, 3).value2
+$return_total+=$ExcelWorkSheet.cells.Item($i, 4).value2
+#$ExcelWorkSheet.Cells.Item($i,1)= $from
+$from+=1
+}
+$i+=2
+$ExcelWorkSheet.Cells.Item($i,1)= 'Total'
+$ExcelWorkSheet.Cells.Item($i,1).Font.Bold=$True
+$ExcelWorkSheet.Cells.Item($i,2)= $credit_total
+$ExcelWorkSheet.Cells.Item($i,3)= $cash_total
+$ExcelWorkSheet.Cells.Item($i,4)= $return_total
+$i+=2
+$ExcelWorkSheet.Cells.Item($i,1)= 'Credit ='
+$ExcelWorkSheet.Cells.Item($i,1).Font.Bold=$True
+$ExcelWorkSheet.Cells.Item($i,2)= $credit_total
+$i+=1
+$ExcelWorkSheet.Cells.Item($i,1)= 'Cash ='
+$ExcelWorkSheet.Cells.Item($i,1).Font.Bold=$True
+$ExcelWorkSheet.Cells.Item($i,2)= $cash_total
+$i+=1
+$ExcelWorkSheet.Cells.Item($i,1)= 'Total Sale ='
+$ExcelWorkSheet.Cells.Item($i,1).Font.Bold=$True
+$ExcelWorkSheet.Cells.Item($i,2)= $cash_total+$credit_total
+$i+=2
+$ExcelWorkSheet.Cells.Item($i,1)= '18% ='
+$ExcelWorkSheet.Cells.Item($i,1).Font.Bold=$True
+$ExcelWorkSheet.Cells.Item($i,2)= $cash_total+$credit_total
+$i+=1
+$ExcelWorkSheet.Cells.Item($i,1)= 'Return ='
+$ExcelWorkSheet.Cells.Item($i,1).Font.Bold=$True
+$ExcelWorkSheet.Cells.Item($i,2)= $return_total
+$wb.Save()
+$wb.close($true)
